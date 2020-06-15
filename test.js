275bcf4ba986facc7de4a30ded69e28f6452e136
Jan Bölsche <regular.gonzales@googlemail.com>
2020-06-15 09:45:13 +0200

    test

--- test.js
+++ test.js
@@ -1,3 +1,5 @@
 const format = require('.')(3)
 
 console.log(format('%ci=%d%c, %cs="%s"%c, %cCSS', 'color:yellow;', 5, 'color:inherit;', 'background: blue;', 'hello','background: inherit', 'color: orange;', 'extra'))
+
+console.log('%j %O', {a: 1}, new Date())
